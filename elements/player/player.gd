915344e68d3b833b9d0c2e326d80da802d8a3c34
Zac Arnold <zacarnold687@gmail.com>
2024-10-17 19:34:24 -0700

    Create hitbox and velocity component

--- elements/player/player.gd
+++ elements/player/player.gd
@@ -1,14 +1,5 @@
 extends CharacterBody2D
 
-@export var movement_speed:float
 
-@onready var state_chart: StateChart = %StateChart
-
-func get_input():
-	var input_direction:Vector2 = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * movement_speed
-
-func _physics_process(delta: float) -> void:
-	get_input()
-	print(velocity)
-	move_and_slide()
+@onready var state_chart : StateChart = %StateChart
+@onready var velocity_component : VelocityComponent  = %VelocityComponent
---
+++ elements/player/velocity_component.gd
@@ -0,0 +1,4 @@
+extends Node2D
+class_name VelocityComponent
+
+@export var MOVE_SPEED : int
